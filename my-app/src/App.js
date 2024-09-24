977faf6238cf811c6cee1142f0b4de43cdd39a76
Habibullah <habibhaseeb716@gmail.com>
2024-09-24 15:11:06 +0500

    header.jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Component from './components/Header/header';
 import React from 'react';
+import Header from './components/Header/header'
 
 function App() {
   return (
