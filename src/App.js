9354b11a6b053d96570f859ae4fc1fb6240940c4
Habibullah <habibhaseeb716@gmail.com>
2024-09-24 14:41:42 +0500

    my-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Component from './components/Header/header';
+import React from 'react';
+
+function App() {
+  return (
+      <Component/>
+  );
+}
+
+export default App;
