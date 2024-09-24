e2d4c4616bc0e907c9f32cb9cb4f4851a279d929
Habibullah <habibhaseeb716@gmail.com>
2024-09-24 14:40:08 +0500

    git push

---
+++ my-app/src/components/Header/header.jsx
@@ -0,0 +1,15 @@
+import Header from '../Header/header.css'
+
+
+function component() {
+    return <>
+    <div className="navbar">
+        <nav className='nav'><ul><li><button>Home</button></li>
+        <li><button>Contacts</button></li>
+        <li><button>Blog</button></li>
+        <li><button>Services</button></li></ul></nav>
+    </div>
+<h1> Welcome to start React.js journey</h1>
+</>
+}
+export default component;
